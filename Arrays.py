2666b9b9da79336c8faf6b0d407f88c95f5c78bb
FynnMc <39341232+FynnMc@users.noreply.github.com>
2019-05-06 13:32:57 +0100

    Completed Hash Class

--- Arrays.py
+++ Arrays.py
@@ -2,7 +2,8 @@ import ctypes
 
 # Dynamic Array Class for creating ArrayList-Like Objects
 
-class DynamicArray():
+
+class DynamicArray:
 
     # Initialise to empty array A of length equal to 1
 
@@ -50,8 +51,52 @@ class DynamicArray():
         return self.A[ind]
 
 
-class Hash():
-    pass
+# HashTable should allow for insertion, deletion, searching and collision
 
 
+class Hash:
 
+    def __init__(self, size, data_list):
+        self.size = size
+        self.hash_table = self.create_hash(data_list)
+
+    def empty_hash(self):
+        e_h = [[] for _ in range(self.size)]
+        return e_h
+
+    def hash_func(self, num):
+        return num % self.size
+
+    def create_hash(self, data):
+        hash_table = self.empty_hash()
+        for i in range(self.size):
+            hash_table[self.hash_func(data[i][0])].append(data[i])
+        return hash_table
+
+    def append(self, data):
+        for i in range(len(data)):
+            self.hash_table[self.hash_func(data[i][0])].append(data[i])
+        return self.hash_table
+
+    def select(self, index):
+        h = self.hash_func(index)
+        bucket = self.hash_table[h]
+        for i in range(len(bucket)):
+            if index == bucket[i][0]:
+                return bucket[i][1]
+        raise KeyError
+
+    def delete(self, index):
+        h = self.hash_func(index)
+        bucket = self.hash_table[h]
+        for i in range(len(bucket)):
+            if index == bucket[i][0]:
+                del bucket[i]
+                return  # Important to add as we cant loop through remainder of deleted bucket
+        raise KeyError
+
+
+test = Hash(4, [(24, 'Hello'), (123541, 'There'), (2, 'World'), (100000003, '!')])
+test.append([(1, 2), (3, 4)])
+print(test.delete(3))
+print(test.hash_table)
